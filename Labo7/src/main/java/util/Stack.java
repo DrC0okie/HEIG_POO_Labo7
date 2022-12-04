fc02884d6841aff3aae2df6e2f377b9ef3ef0609
DrC0okie <timothee.vanhove@heig-vd.ch>
2022-12-04 09:42:59 +0100

    added a comment

--- Labo7/src/main/java/util/Stack.java
+++ Labo7/src/main/java/util/Stack.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Represents a LIFO stack of objects.
+ * Represents a LIFO stack of objects where each item has a reference to the next one.
  */
 public class Stack {
     /**
